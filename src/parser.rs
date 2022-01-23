7ffd7deabc3669f661fb28205a5e40fe6151bfcd
Nils Feierabend <mztikk@outlook.de>
2022-01-23 15:38:38 +0100

    removed optimizations in parser, let the optimizer handle that

--- src/parser.rs
+++ src/parser.rs
@@ -21,46 +21,13 @@ impl Parser {
 
     pub fn parse_instructions(source: &[char]) -> Vec<Instruction> {
         let mut result = Vec::new();
-        let mut iter = source.iter().peekable();
 
-        while let Some(opcode) = iter.next() {
+        for opcode in source.iter() {
             match opcode {
-                '>' => {
-                    let mut count = 1;
-                    while iter.peek() == Some(&&'>') {
-                        iter.next();
-                        count += 1;
-                    }
-
-                    result.push(Instruction::FWD(count));
-                }
-                '<' => {
-                    let mut count = 1;
-                    while iter.peek() == Some(&&'<') {
-                        iter.next();
-                        count += 1;
-                    }
-
-                    result.push(Instruction::BAK(count));
-                }
-                '+' => {
-                    let mut count = 1;
-                    while iter.peek() == Some(&&'+') {
-                        iter.next();
-                        count += 1;
-                    }
-
-                    result.push(Instruction::INC(count))
-                }
-                '-' => {
-                    let mut count = 1;
-                    while iter.peek() == Some(&&'-') {
-                        iter.next();
-                        count += 1;
-                    }
-
-                    result.push(Instruction::DEC(count))
-                }
+                '>' => result.push(Instruction::FWD(1)),
+                '<' => result.push(Instruction::BAK(1)),
+                '+' => result.push(Instruction::INC(1)),
+                '-' => result.push(Instruction::DEC(1)),
                 '.' => result.push(Instruction::OUT),
                 ',' => result.push(Instruction::IN),
                 '[' => result.push(Instruction::IF),
@@ -68,27 +35,6 @@ impl Parser {
                 _ => panic!("Unknown instruction {}", opcode),
             }
         }
-
-        // for opcode in iter {
-        //     match opcode {
-        //         '>' => result.push(Instruction::FWD(1)),
-        //         '<' => result.push(Instruction::BAK(1)),
-        //         '+' => {
-        //             let mut count = 1;
-        //             while iter.peek() == Some(&'+') {
-        //                 iter.next();
-        //                 count += 1;
-        //             }
-        //             result.push(Instruction::INC(count))
-        //         },
-        //         '-' => result.push(Instruction::DEC(1)),
-        //         '.' => result.push(Instruction::OUT),
-        //         ',' => result.push(Instruction::IN),
-        //         '[' => result.push(Instruction::IF),
-        //         ']' => result.push(Instruction::EIF),
-        //         _ => panic!("Unknown instruction {}", opcode),
-        //     }
-        // }
 
         result
     }
