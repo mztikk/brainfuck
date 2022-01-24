94c3edbcd8f51649e6c9f7cccded49131e8aae7a
mztikk <mztikk@outlook.de>
2022-01-24 23:16:26 +0100

    add: added more parser tests

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -75,3 +75,77 @@ fn test_parse_all() {
         ]
     );
 }
+
+#[test]
+fn test_parse_all_with_whitespace() {
+    let source = "> + < - . , [ ]";
+    let instructions = Parser::parse_instructions(&Parser::parse_string(source));
+    assert_eq!(
+        instructions,
+        vec![
+            Instruction::FWD(1),
+            Instruction::INC(1),
+            Instruction::BAK(1),
+            Instruction::DEC(1),
+            Instruction::OUT,
+            Instruction::IN,
+            Instruction::IF,
+            Instruction::EIF,
+        ]
+    );
+}
+
+#[test]
+fn test_parse_all_with_other() {
+    let source = "> cd+nh <_ -: .; , [ ] # ignored !23455§$%&/()=?";
+    let instructions = Parser::parse_instructions(&Parser::parse_string(source));
+    assert_eq!(
+        instructions,
+        vec![
+            Instruction::FWD(1),
+            Instruction::INC(1),
+            Instruction::BAK(1),
+            Instruction::DEC(1),
+            Instruction::OUT,
+            Instruction::IN,
+            Instruction::IF,
+            Instruction::EIF,
+        ]
+    );
+}
+
+#[test]
+fn test_parse_inc_multiple() {
+    for i in 0..250 {
+        let source = "+".repeat(i);
+        let instructions = Parser::parse_instructions(&Parser::parse_string(source.as_str()));
+        assert_eq!(instructions, vec![Instruction::INC(1); i]);
+    }
+}
+
+#[test]
+fn test_parse_dec_multiple() {
+    for i in 0..250 {
+        let source = "-".repeat(i);
+        let instructions = Parser::parse_instructions(&Parser::parse_string(source.as_str()));
+        assert_eq!(instructions, vec![Instruction::DEC(1); i]);
+    }
+}
+
+#[test]
+fn test_parse_fwd_multiple() {
+    for i in 0..250 {
+        let source = ">".repeat(i);
+        let instructions = Parser::parse_instructions(&Parser::parse_string(source.as_str()));
+        assert_eq!(instructions, vec![Instruction::FWD(1); i]);
+    }
+}
+
+#[test]
+fn test_parse_bak_multiple() {
+    for i in 0..250 {
+        let source = "<".repeat(i);
+        let instructions = Parser::parse_instructions(&Parser::parse_string(source.as_str()));
+        assert_eq!(instructions, vec![Instruction::BAK(1); i]);
+    }
+}
